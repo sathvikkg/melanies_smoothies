c5202ef560d6fb735ea2b7acecfcadb782a7a98e
Sathvik Reddy Gangula <154527808+sathvikkg@users.noreply.github.com>
2026-06-05 17:45:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,11 @@ VALUES
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+if ingredients_list:
+  ingredients_string = ''
+
+for fruit_chosen in ingredients_list:
+  ingredients_string += fruit_chosen + ' '
+  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+  sf_df st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
